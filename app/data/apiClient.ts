cea6e60406e4c8ef63f32e522b21cd5b6ca8875d
Kevin Tillot <kevin.tillot@gmail.com>
2021-12-24 16:18:47 +0100

    baseURL => github

--- app/data/apiClient.ts
+++ app/data/apiClient.ts
@@ -1,27 +1,37 @@
-//@ts-ignore
-const baseURL = API_URL || "http://localhost:5000";
+const baseURL = "https://superchauzette.github.io/api-kompa/api";
 
 export async function getArtists(): Promise<Artist[]> {
-  const res = await fetch(baseURL + "/artists.json");
-  const artists: Artist[] = await res.json();
-  return artists;
+  try {
+    const res = await fetch(baseURL + "/artists.json");
+    const artists: Artist[] = await res.json();
+    return artists;
+  } catch {
+    return [];
+  }
 }
 
 export async function getAlbumsByArtist(artist: string): Promise<Album[]> {
-  const res = await fetch(baseURL + "/albums.json");
-  const albums: Album[] = await res.json();
-  const albumsByArtist = albums
-    .filter((a) => a.slugArtist === artist)
-    ?.map((a) => ({ ...a, url: `${baseURL}${a.url}` }));
-  if (!albumsByArtist) throw new Response("Rien trouvé", { status: 404 });
-  return albumsByArtist;
+  try {
+    const res = await fetch(baseURL + "/albums.json");
+    const albums: Album[] = await res.json();
+    const albumsByArtist = albums
+      .filter((a) => a.slugArtist === artist)
+      ?.map((a) => ({ ...a, url: `${baseURL}${a.url}` }));
+
+    if (!albumsByArtist) throw new Response("Rien trouvé", { status: 404 });
+    return albumsByArtist;
+  } catch {
+    return [];
+  }
 }
 
 export async function getAlbumBySlug(slugArtist: string, slugAlbum: string): Promise<Album> {
   const albums = await getAlbumsByArtist(slugArtist);
   const album = albums.find((a) => a.slugAlbum === slugAlbum);
   if (!album) throw new Response("Rien trouvé", { status: 404 });
-  return album;
+  const toto = { ...album, tracks: album.tracks.map((t) => ({ ...t, url: `${baseURL}${t.url}` })) };
+  console.log({ toto });
+  return toto;
 }
 
 export type Artist = {
